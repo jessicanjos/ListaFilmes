aa4190985ac9679007b43a05987c796f34345684
jessicanjos <jessicanjos2003@gmail.com>
2022-12-01 20:24:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -0,0 +1,50 @@
+package br.com.etecia.listafilmes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listaFilmes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaFilmes = findViewById(R.id.idlistaFilmes);
+
+        ,MyAdapter adapter = new MyAdapter();
+
+        //preparar para instanciar o adaptador
+        listaFilmes.setAdapter(Adaptador);
+    }
+    //Criando uma InnerClass MyAdapter
+
+    public class MyAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+}
