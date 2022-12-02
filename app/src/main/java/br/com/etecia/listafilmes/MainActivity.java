dde6be9cc9204c547032c77c89b8a4a5d6f9dd54
jessicanjos <jessicanjos2003@gmail.com>
2022-12-01 22:17:40 -0300

    Lista de filmes

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView listaFilmes;
+
+    //Criar a base de dados para os filmes
+
+    String titulo[] = {"2067","A CULPA É DAS ESTRELAS", "LIVRO DE ELI", "VINGADORES ULTIMATO"};
+    String descricao[] = {"A Terra está à beira da extinção. Após anos de mudança climática, a vida vegetal desapareceu e o oxigênio tornou-se escasso.","Diagnosticada com câncer, a adolescente Hazel Grace Lancaster (Shailene Woodley) se mantém viva graças a uma droga experimental.", "Em um futuro não muito distante, 30 anos após o término da última guerra. Eli (Denzel Washington) é um homem solitário, que percorre a América do Norte devastada.", "Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos."};
+    String acesso[] = {};
+    int filmes[] = {R.drawable.filmeUm, R.drawable.aCulpaEdasEstrelas, R.drawable.filmeTres, R.drawable.filmeQuatro};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return filmes.length;
         }
 
         @Override
@@ -44,7 +54,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+
+            ImageView imagemFilme;
+            TextView tituloFilme, descricaoFilme, acessoFilme;
+
+            //Adaptador ligando ao modelo
+
+            View v = getLayoutInflater().inflate();
+
+            imagemFilme = v.findViewById(R.id.imgFilmes);
+            tituloFilme = v.findViewById(R.id.idTituloFilmes);
+            descricaoFilme = v.findViewById(R.id.idDescricaoFilmes);
+            acessoFilme = v.findViewById(R.id.idAcessoFilmes);
+
+            tituloFilme.setText(titulo[position]);
+            descricaoFilme.setText(titulo[position]);
+            imagemFilme.setImageResource(titulo[position]);
+            acessoFilme.setText(titulo[position]);
+
+            return v;
         }
     }
 }
